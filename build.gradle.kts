2ec643d5de4a0bf1e424e40ef6703387b7bc7ea2
JungleMystic <locharla.rammohan@gmail.com>
2023-09-06 17:23:24 +0530

    Initial Commit v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,6 @@ buildscript {
 
 plugins {
     id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id ("com.google.devtools.ksp") version "1.6.10-1.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id ("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
